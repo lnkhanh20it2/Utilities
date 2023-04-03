24efec632b6ab5267fc8933a0fe1b9b0c3d8145c
Khanh <lnkhanh.20it2@vku.udn.vn>
2023-04-03 17:15:07 +0700

    Glide

--- app/src/main/java/com/example/utilities/MainActivity.kt
+++ app/src/main/java/com/example/utilities/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.utilities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import coil.load
-import coil.size.Scale
-import coil.transform.CircleCropTransformation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.utilities.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val imgUrl = "https://images.unsplash.com/photo-1677784976154-816c3ceca511?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4MDUxNjM1MQ&ixlib=rb-4.0.3&q=80&w=1080"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Glide.with(applicationContext)
+            .load(imgUrl)
+            .transform(CircleCrop())
+            .into(binding.imgView)
     }
 }
