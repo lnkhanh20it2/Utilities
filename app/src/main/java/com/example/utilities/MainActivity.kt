e917bc2280cc6ffbba80f500d9c293fa9565b34f
Khanh <lnkhanh.20it2@vku.udn.vn>
2023-04-03 22:23:46 +0700

    Picasso

--- app/src/main/java/com/example/utilities/MainActivity.kt
+++ app/src/main/java/com/example/utilities/MainActivity.kt
@@ -2,16 +2,18 @@ package com.example.utilities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import coil.load
-import coil.size.Scale
-import coil.transform.CircleCropTransformation
 import com.example.utilities.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val imgUrl = "https://picsum.photos/200/300"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Picasso.get()
+            .load(imgUrl)
+            .into(binding.imgView)
     }
 }
