4ebf5ed090db40911d46de6544c0c3fa2964b65d
Khanh <lnkhanh.20it2@vku.udn.vn>
2023-04-04 13:26:32 +0700

    Fresco

--- app/src/main/java/com/example/utilities/MainActivity.kt
+++ app/src/main/java/com/example/utilities/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.utilities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import coil.load
-import coil.size.Scale
-import coil.transform.CircleCropTransformation
 import com.example.utilities.databinding.ActivityMainBinding
+import com.facebook.drawee.backends.pipeline.Fresco
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val imgUrl = "https://fastly.picsum.photos/id/201/200/300.jpg?grayscale&hmac=BgHJjRdLh_COHXoc1t-UQZ-iPISHfWJ_kkRqeYpti4o"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fresco.initialize(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.imgView.setImageURI(Uri.parse(imgUrl))
     }
 }
