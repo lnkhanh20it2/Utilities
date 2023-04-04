4f6b718babcb62419c684d087d059fa28fb3faef
Khanh <lnkhanh.20it2@vku.udn.vn>
2023-04-04 13:49:52 +0700

    PhotoView

--- app/src/main/java/com/example/utilities/MainActivity.kt
+++ app/src/main/java/com/example/utilities/MainActivity.kt
@@ -2,16 +2,18 @@ package com.example.utilities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import coil.load
-import coil.size.Scale
-import coil.transform.CircleCropTransformation
+import com.bumptech.glide.Glide
 import com.example.utilities.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val imgUrl = "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        Glide.with(applicationContext)
+            .load(imgUrl)
+            .into(binding.photoView)
     }
 }
